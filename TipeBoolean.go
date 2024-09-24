d09dbd75a9cafd0d0ff1e0dd03892f4f2a0bbf1c
spnirma <89586462+spnirma@users.noreply.github.com>
2024-09-24 18:03:23 +0700

    Create TipeBoolean.go

---
+++ TipeBoolean.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Deklarasi variabel boolean
+    isRaining := true
+
+    // Menampilkan nilai boolean
+    fmt.Println("Apakah sedang hujan?", isRaining)
+
+    // Mengubah nilai boolean
+    isRaining = false
+    fmt.Println("Apakah sedang hujan sekarang?", isRaining)
+}
